d52cc47687f8a4ac3d6c500d6cf10f5ccb12e694
unknown <nicolas.prieto-v@labinfo.is.escuelaing.edu.co>
2025-10-21 10:47:42 -0500

    proxy finalizado

--- proxy/src/main/java/com/example/demo/controller/HttpConnectionExample.java
+++ proxy/src/main/java/com/example/demo/controller/HttpConnectionExample.java
@@ -1,17 +1,20 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Service;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+@Service
 public class HttpConnectionExample {
 
     private static final String USER_AGENT = "Mozilla/5.0";
-    public static void main(String[] args) throws IOException {
+    public String get(String url) throws IOException {
 
-        URL obj = new URL(GET_URL);
+        URL obj = new URL(url);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
         con.setRequestMethod("GET");
         con.setRequestProperty("User-Agent", USER_AGENT);
@@ -34,9 +37,9 @@ public class HttpConnectionExample {
             // print result
             System.out.println(response.toString());
         } else {
-            System.out.println("GET request not worked");
+            return ("GET request not worked");
         }
-        System.out.println("GET DONE");
+        return ("GET DONE");
     }
 
 }
