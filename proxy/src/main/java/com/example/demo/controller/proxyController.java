57d45e95f8f846f734d5db2b798057987164971e
unknown <nicolas.prieto-v@labinfo.is.escuelaing.edu.co>
2025-10-21 10:02:43 -0500

    commit

--- proxy/src/main/java/com/example/demo/controller/proxyController.java
+++ proxy/src/main/java/com/example/demo/controller/proxyController.java
@@ -1,10 +1,15 @@
 package com.example.demo.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class proxyController {
     int index = 0;
+    String urls[]= {"http://23.20.105.209:8080", "http://35.171.112.228:8080"};
+    @Autowired index = (index + 1) % urls.length();
+
+
     
 }
